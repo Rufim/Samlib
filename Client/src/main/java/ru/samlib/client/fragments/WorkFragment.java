a39bc3fc694cbdece93686a5ba0b0909a63a5ef1
Dmitry <0shadow0@mail.ru>
2017-05-14 18:21:00 +0300

    add ui for tts

--- Client/src/main/java/ru/samlib/client/fragments/WorkFragment.java
+++ Client/src/main/java/ru/samlib/client/fragments/WorkFragment.java
@@ -305,7 +305,7 @@ public class WorkFragment extends ListFragment<String> implements View.OnClickLi
         if (autoScroller != null) autoScroller.cancel();
         autoScroller = new CountDownTimer(totalScrollTime, scrollPeriod) {
             public void onTick(long millisUntilFinished) {
-                itemList.scrollBy(0, (int) (heightToScroll * (getRate(autoScrollSpeed) * 0.07)));
+                itemList.scrollBy(0, (int) (heightToScroll * (getRate(autoScrollSpeed) * 0.25)));
             }
 
             public void onFinish() {
